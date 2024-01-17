c098fc94aec867d6612b07bd2843f7df6174bfe1
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2024-01-17 19:21:57 +0000

    Added some Carousel updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
