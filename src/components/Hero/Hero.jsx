dcaca4d8b55bd1ca710637c44cd5c05d477dc088
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2024-01-17 19:20:01 +0000

    Router DOM fix

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Hero.css";
+
+function Hero() {
+    return (
+        <section className="container">
+            <div className="content"></div>            
+        </section>
+    )
+}
+
+export default Hero;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Carousel from "../../components/Carousel/Carousel";
+import Hero from "../../components/Hero/Hero";
 
 import "./Home.css";
 
 function Home() {
     return (
         <div className="home-page">
-            <h1>Home Title Placeholder</h1>
+            <Hero />
             <Carousel />
         </div>
     );
