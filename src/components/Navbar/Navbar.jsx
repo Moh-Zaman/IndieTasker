bbe0fced414efaa2edd19dabddc81b46b3e059c6
Emma Blencowe <emma.blencowe95@gmail.com>
2024-01-17 18:32:30 +0000

    Began adding some css styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,20 @@ import "./Navbar.css"
 
 const Navbar = () => {
     return (
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="container">
+                <div className="logo">
+                    <span className="text">IndieTasker</span>
+                </div>
+                <div className="links">
+                    {/* this needs work, not sure what we're linking yet */}
+                    <span>About</span>
+                    <span>Home</span>
+                    <span>Freelancer?</span>
+                    <button>Sign In</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
