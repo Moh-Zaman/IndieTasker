b26ffbbd7465c5fec061227f9948c5ddb32c59eb
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2024-01-18 22:33:18 +0000

    Added arrows for the carousel and some styling. Not functional yet.

--- src/components/FreelancerCard/FreelancerCard.jsx
+++ src/components/FreelancerCard/FreelancerCard.jsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+
 import "./FreelancerCard.css";
 
 function FreelancerCard(props) {
