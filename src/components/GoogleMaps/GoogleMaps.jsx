57f283bccc19a6d5cbc167b8b5e1344ad8cd7938
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2024-01-18 19:52:34 +0000

    Added components for google maps, will change branches after merge

---
+++ src/components/GoogleMaps/GoogleMaps.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useRef, useEffect, useState } from "react";
+
+import "./GoogleMaps.css"
+
+function GoogleMaps() {
+
+
+    return (
+        <div></div>
+    )
+}
