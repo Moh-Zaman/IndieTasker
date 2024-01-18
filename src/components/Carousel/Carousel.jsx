c5a3a7faa5d02aec22bfdfdfc1b38f7703c4ec68
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2024-01-18 20:33:29 +0000

    Added some freelancer card styling and installed axios

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -20,35 +20,43 @@ function Carousel() {
     }, []);
 
     return (
-        <motion.div ref={carousel} className="carousel-container">
-            <motion.div ref={innerCarousel} drag="x" dragConstraints={{ right: 150, left: -width }} initial={{ x: 150 }} className="horizontal-carousel">
-                <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
-                    <FreelancerCard title="Designer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Designer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Designer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                </motion.div>
-                <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
-                    <FreelancerCard title="Developer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Developer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Developer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                </motion.div>
-                <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
-                    <FreelancerCard title="Photographer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Photographer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Photographer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                </motion.div>
-                <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
-                    <FreelancerCard title="Translator" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Translator" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Translator" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                </motion.div>
-                <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
-                    <FreelancerCard title="Writer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Writer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
-                    <FreelancerCard title="Writer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+        <div className="carousel-parent">
+            <button className="carousel-button" id="carousel-previous">
+                ←
+            </button>
+            <motion.div ref={carousel} className="carousel-container">
+                <motion.div ref={innerCarousel} drag="x" dragConstraints={{ right: 0, left: -width }} initial={{ x: 0 }} className="horizontal-carousel">
+                    <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
+                        <FreelancerCard title="Designer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Designer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Designer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                    </motion.div>
+                    <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
+                        <FreelancerCard title="Developer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Developer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Developer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                    </motion.div>
+                    <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
+                        <FreelancerCard title="Photographer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Photographer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Photographer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                    </motion.div>
+                    <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
+                        <FreelancerCard title="Translator" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Translator" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Translator" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                    </motion.div>
+                    <motion.div drag="y" dragConstraints={{ bottom: 0, top: -height - 15 }} className="vertical-carousel">
+                        <FreelancerCard title="Writer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Writer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                        <FreelancerCard title="Writer" image="https://picsum.photos/250/200" text="Lorem ipsum is placeholder text commonly used in the graphic, print..." />
+                    </motion.div>
                 </motion.div>
             </motion.div>
-        </motion.div>
+            <button className="carousel-button" id="carousel-next">
+                →
+            </button>
+        </div>
     );
 }
 
