58ac763b2d8b0096a7144f14a53d67f3cbc2e58e
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2024-01-21 16:01:14 +0000

    Improved the carousel design so it's more like the mockup. (not yet responsive)

--- src/components/FreelancerCard/FreelancerCard.jsx
+++ src/components/FreelancerCard/FreelancerCard.jsx
@@ -2,41 +2,42 @@ import "./FreelancerCard.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function FreelancerCard(props) {
-
+function FreelancerCard() {
     const [Users, setUsers] = useState([]);
     useEffect(() => {
-      axios.get("https://randomuser.me/api/?results=25").then((Response) => {
-        if (Response.data) {
-          console.log("FOund");
-          setUsers(Response.data.results);
-        } else {
-          console.log("not found");
-        }
-      }).catch(error => console.log(error));
+        axios
+            .get("https://randomuser.me/api/?results=1")
+            .then((Response) => {
+                if (Response.data) {
+                    console.log("FOund");
+                    setUsers(Response.data.results);
+                } else {
+                    console.log("not found");
+                }
+            })
+            .catch((error) => console.log(error));
     }, []);
 
     const displaylist = Users.map((User, index) => {
-      return (
-
-        <div className="freelancer-card">
-            <h3 key={index}className="freelancer-card-title">{User.name.first + " " + User.name.last}</h3>
-            <img key={index} className="freelancer-card-image" src={User.picture.large}  alt="" />
-            <div key={index} className="freelancer-card-body">
-                <p key={index} className="freelancer-card-text">{User.email}</p>
-                <button key={index} className="freelancer-card-button">Hire Me</button>
+        return (
+            <div className="freelancer-card">
+                <h3 key={index} className="freelancer-card-title">
+                    {User.name.first + " " + User.name.last}
+                </h3>
+                <img key={index} className="freelancer-card-image" src={User.picture.large} alt="" />
+                <div key={index} className="freelancer-card-body">
+                    <p key={index} className="freelancer-card-text">
+                        {User.email}
+                    </p>
+                    <button key={index} className="freelancer-card-button">
+                        Hire Me
+                    </button>
+                </div>
             </div>
-        </div>
-
-      )
+        );
     });
- 
 
-    return (
-        <div>{displaylist}</div>        
-    );
+    return <div>{displaylist}</div>;
 }
 
 export default FreelancerCard;
-
-
