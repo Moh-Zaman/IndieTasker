ca21a24636e529ee27409eaef6407af32a6fcb44
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2024-01-21 17:06:33 +0000

    added carpusel buttons

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -17,6 +17,8 @@ function Carousel() {
     const [positionIndex, setPositionIndex] = useState([0, 1, 2]);
     const [positionIndex1, setPositionIndex1] = useState([0, 1, 2]);
     const [positionIndex2, setPositionIndex2] = useState([0, 1, 2]);
+
+    const left1 = useRef();
 
     function handleNext() {
         setPositionIndex((prevIndex) => {
@@ -82,34 +84,85 @@ function Carousel() {
     };
 
     return (
-        <div className="carousel-container">
-            <div className="carousel">
-                <h3 className="freelancer-type">Designers</h3>
-                {cards.map((card, index) => (
-                    <motion.div onPan={sleepScroll} key={index} className="item" initial="center" animate={positions[positionIndex[index]]} variants={variants} transition={{ duration: 0.5 }}>
-                        {card}
-                    </motion.div>
-                ))}
-            </div>
+        <>
+            <h1 className="featured-title">Featured Taskers</h1>
+            <div className="carousel-container">
+                <div className="carousel">
+                    <h3 className="freelancer-type">Carpenters</h3>
 
-            <div className="carousel">
-                <h3 className="freelancer-type">Developers</h3>
-                {cards1.map((card, index) => (
-                    <motion.div onPan={sleepScroll1} key={index} className="item" initial="center" animate={positions[positionIndex1[index]]} variants={variants} transition={{ duration: 0.5 }}>
-                        {card}
-                    </motion.div>
-                ))}
-            </div>
+                    <i ref={left1} className="left-arrow" onClick={sleepScroll}>
+                        {leftArrow}
+                    </i>
 
-            <div className="carousel">
-                <h3 className="freelancer-type">Translators</h3>
-                {cards2.map((card, index) => (
-                    <motion.div onPan={sleepScroll2} key={index} className="item" initial="center" animate={positions[positionIndex2[index]]} variants={variants} transition={{ duration: 0.5 }}>
-                        {card}
-                    </motion.div>
-                ))}
+                    {cards.map((card, index) => (
+                        <motion.div
+                            onPan={sleepScroll}
+                            key={index + "0"}
+                            className="item"
+                            initial="center"
+                            animate={positions[positionIndex[index]]}
+                            variants={variants}
+                            transition={{ duration: 0.5 }}>
+                            {card}
+                        </motion.div>
+                    ))}
+
+                    <i className="right-arrow" onClick={sleepScroll}>
+                        {rightArrow}
+                    </i>
+                </div>
+
+                <div className="carousel">
+                    <h3 className="freelancer-type">Developers</h3>
+
+                    <i className="left-arrow" onClick={sleepScroll1}>
+                        {leftArrow}
+                    </i>
+
+                    {cards1.map((card, index) => (
+                        <motion.div
+                            onPan={sleepScroll1}
+                            key={index + "1"}
+                            className="item"
+                            initial="center"
+                            animate={positions[positionIndex1[index]]}
+                            variants={variants}
+                            transition={{ duration: 0.5 }}>
+                            {card}
+                        </motion.div>
+                    ))}
+
+                    <i className="right-arrow" onClick={sleepScroll1}>
+                        {rightArrow}
+                    </i>
+                </div>
+
+                <div className="carousel">
+                    <h3 className="freelancer-type">Translators</h3>
+
+                    <i className="left-arrow" onClick={sleepScroll2}>
+                        {leftArrow}
+                    </i>
+
+                    {cards2.map((card, index) => (
+                        <motion.div
+                            onPan={sleepScroll2}
+                            key={index + "2"}
+                            className="item"
+                            initial="center"
+                            animate={positions[positionIndex2[index]]}
+                            variants={variants}
+                            transition={{ duration: 0.5 }}>
+                            {card}
+                        </motion.div>
+                    ))}
+
+                    <i className="right-arrow" onClick={sleepScroll2}>
+                        {rightArrow}
+                    </i>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
