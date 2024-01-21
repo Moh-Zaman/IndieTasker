1dbadc70fa8c5c9c0676134bd79a0f741c09fa80
Greg Duke <105805424+SatsumaSegment@users.noreply.github.com>
2024-01-21 14:50:28 +0000

    fixed server posting issues

--- server/server.js
+++ server/server.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const sqlite3 = require('sqlite3').verbose();
+import express from "express";
+const app = express();
+import cors from "cors";
+import sqlite3 from "sqlite3";
 
-
-app.use(cors())
+app.use(cors());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Alow-Origin", "*");
     next();
 });
-app.use(express.json({limit:'10mb'}))
+app.use(express.json({ limit: "10mb" }));
 
-let db = new sqlite3.Database('credentials.', (err) => {
-    if(err) {
+let db = new sqlite3.Database("credentials.db", (err) => {
+    if (err) {
         console.log(err.message);
     }
-    console.log('Connected to the database')
-})
+    console.log("Connected to the database");
+});
 
-app.post('/validatePassword', (req, res) => {
-    const {username, password} = req.body
-    db.all(`select * from credentials where username = '${username}' and password = '${password}'`, (err, rows) =>{
-        if(err){
+app.post("/validatePassword", (req, res) => {
+    const { username, password } = req.body;
+    db.all(`select * from credentials where username = '${username}' and password = '${password}'`, (err, rows) => {
+        if (err) {
             throw err;
         }
-        if(rows.length > 0){
-            res.send({validation: true})
-        }else{
-            res.send({validation:false})
+        if (rows.length > 0) {
+            res.send({ validation: true });
+        } else {
+            res.send({ validation: false });
         }
-    })
-})
+    });
+});
 
-
-
-
-
-app.listen(2001, () => console.log('Listening to 3001 port'))
+app.listen(3001, "localhost"); // or server.listen(3001, '0.0.0.0'); for all interfaces
+app.on("listening", function () {
+    console.log("Express server started on port %s at %s", server.address().port, server.address().address);
+});
--- src/components/Login/Auth.jsx
+++ src/components/Login/Auth.jsx
@@ -1,75 +1,65 @@
 import React from "react";
-import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Form, Input, Button, Checkbox } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import axios from "axios";
 
-
 export default function Auth() {
-    const onFinish = value => {
-        const {username,password} = value
-        axios.post('https://localhost:3001/validatePassword', {username, password})
-        .then(res=>{
-            if(res.data.validation){
-              alert('Password matches')
+    const onFinish = (value) => {
+        const { username, password } = value;
+        axios.post("http://localhost:3001/validatePassword", { username, password }).then((res) => {
+            if (res.data.validation) {
+                alert("Password matches");
             } else {
-              alert('Password bad!')
+                alert("Password bad!");
             }
-        }) 
-    }
+        });
+    };
     return (
         <div>
-                <Form
-      name="normal_login"
-      className="login-form"
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}>
+                <Form.Item
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your Username!",
+                        },
+                    ]}>
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: "Please input your Password!",
+                        },
+                    ]}>
+                    <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
+                </Form.Item>
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>Remember me</Checkbox>
+                    </Form.Item>
 
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
+                    <a className="login-form-forgot" href="">
+                        Forgot password
+                    </a>
+                </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
-
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        Log in
+                    </Button>
+                    Or <a href="">register now!</a>
+                </Form.Item>
+            </Form>
         </div>
-    )
+    );
 }
