99770fe5f94019dbfdafbaf31fb9dabd02dfed80
Emma Blencowe <emma.blencowe95@gmail.com>
2024-01-22 19:08:31 +0000

    Fixed some media queries for navbar so buttons arent squished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,50 +1,52 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
-    const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-    return (
-        <nav className="navigation">
-            <div className="brand-name">IndieTasker</div>
-            <button
-                className="hamburger"
-                onClick={() => {
-                    setIsNavExpanded(!isNavExpanded);
-                }}>
-                {/* icon from Heroicons.com */}
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
-                    <path
-                        fillRule="evenodd"
-                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-                        clipRule="evenodd"
-                    />
-                </svg>
-            </button>
-            <div className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
-                <ul>
-                    <li>
-                        <a href="/">Home</a>
-                    </li>
-                    <li>
-                        <a href="/about">About</a>
-                    </li>
-                    <li>
-                    <a href="/profile">My Profile</a>
-                    </li>
-                    <li>
-                    <a className="sign-up-button" href="/register">
-                            Sign Up
-                        </a>
-                    </li>
-                    <li>
-                        <a className="log-in-button" href="/login">Log In</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    );
+  const toggleNav = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
+  return (
+    <nav className="navigation">
+      <div className="brand-name">IndieTasker</div>
+      <button
+        className="hamburger"
+        onClick={toggleNav}
+      >
+        {/* icon from Heroicons.com */}
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div className={isNavExpanded ? "navigation-menu expanded" : "navigation-menu"}>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/profile">My Profile</a>
+          </li>
+          <li>
+            <a className="sign-up-button" href="/register">
+              Sign Up
+            </a>
+          </li>
+          <li>
+            <a className="log-in-button" href="/login">Log In</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
