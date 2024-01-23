ae7d0931d39cc21af9672a37f3e2a47a3f200081
Emma Blencowe <146027243+embleem@users.noreply.github.com>
2024-01-23 21:44:01 +0000

    Added Slide & Category Card components, working on functionality still (#69)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,17 @@
 import Carousel from "../../components/Carousel/Carousel";
 import Hero from "../../components/Hero/Hero";
-
+import Slide from "../../components/Slide/Slide";
 
 import "./Home.css";
 
 function Home() {
-    return (
-        <div className="home-page">
-            <Hero />
-            <Carousel />
-        </div>
-    );
+  return (
+    <div className="home-page">
+        <Hero />
+        <Slide />
+        <Carousel />
+    </div>
+  );
 }
 
 export default Home;
