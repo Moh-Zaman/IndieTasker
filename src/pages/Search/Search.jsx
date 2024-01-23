dd6f3469a878709c20f1d417c87f2fbb921dedf7
Moh-Zaman <145913815+Moh-Zaman@users.noreply.github.com>
2024-01-23 21:58:43 +0000

    search cards functioning

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FreelanceSearch from '../../components/FreelanceSearch/FreelanceSearch'
+
+function Search() {
+  return (
+      <FreelanceSearch />
+  )
+}
+
+export default Search
