b87876a3fad4ef5cbbc208bc588bf3ef47fe1d23
Harry Vaikkath <hvaikkath20@gmail.com>
2024-01-23 04:49:06 +0000

    Updated two FAQs

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -8,27 +8,15 @@ function FAQ() {
       <h2 className="faq-title">Frequently Asked Questions</h2>
       <Accordion>
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Accordion Item #1</Accordion.Header>
+          <Accordion.Header>What is IndieTasker, and how does it work?</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+          IndieTasker is an online platform connecting freelancers with clients seeking a wide range of services. Freelancers showcase their skills and expertise, while clients browse profiles to find the perfect match for their projects. The platform facilitates communication, project collaboration, and secure transactions.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="1">
-          <Accordion.Header>Accordion Item #2</Accordion.Header>
+          <Accordion.Header>How do I get started as a freelancer on IndieTasker?</Accordion.Header>
           <Accordion.Body>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+          To join IndieTasker as a freelancer, simply sign up for an account, create a comprehensive profile highlighting your skills and experience, and start browsing available projects. Be sure to set your rates, availability, and complete your profile to increase your visibility to potential clients.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="2">
