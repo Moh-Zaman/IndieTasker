4315905e2d1b80e1c4334f00ca0aa0cf7d803828
Emma Blencowe <146027243+embleem@users.noreply.github.com>
2024-01-25 01:15:30 +0000

    Feature categories (#89)
    * Added category to JSON profile file
    
    * Updated gigImages JSON to only include Tech Job images
    
    * Changed carousel class names so ReactBootstrap works
    
    * Centered Featured Categories
    
    * Reverted to old carousel in SLIDER component

--- src/components/CatCard/CatCard.jsx
+++ src/components/CatCard/CatCard.jsx
@@ -1,27 +1,62 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./CatCard.css";
-import { MDBCarousel, MDBCarouselItem, MDBCarouselCaption } from 'mdb-react-ui-kit';
-
-import catArr from "../../data/catCardData.json"
 
 function CatCard() {
+  const [Users, setUsers] = useState([]);
+  useEffect(() => {
+      axios
+          .get("https://randomuser.me/api/?results=1")
+          .then((Response) => {
+              if (Response.data) {
+                  console.log("Found");
+                  setUsers(Response.data.results);
+              } else {
+                  console.log("not found");
+              }
+          })
+          .catch((error) => console.log(error));
+  }, []);
 
-   console.log(catArr)
+  const displaylist = Users.map((User, index) => {
+      return (     
+          <div key={index} className="cat-card">
+              <h3 className="cat-card-title">{User.name.first + " " + User.name.last}</h3>
+              <img className="cat-card-image" src={User.picture.large} alt="" />
+              <div className="cat-card-body">
+                  <p className="cat-card-text">{User.email}</p>
+                  <button className="cat-card-button">Hire Me</button>
+              </div>
+          </div>
+      );
+  });
 
-
-
-   return(
-    <MDBCarousel showIndicators showControls fade>
-      {catArr.map((item) => (
-        <MDBCarouselItem itemId={item.id}>
-            <img src={item.img} className='d-block w-100' alt='...' />
-            <MDBCarouselCaption>
-            <h5>{item.title}</h5>
-            <p>{item.desc}</p>
-            </MDBCarouselCaption>
-        </MDBCarouselItem>
-      ))}
-    </MDBCarousel>    
-   )
+  return <div>{displaylist}</div>;
 }
 
 export default CatCard;
+
+// import catArr from "../../data/catCardData.json";
+
+// function CatCard() {
+//   console.log(catArr);
+
+//   return (
+//     <div className="cat-card-wrapper">
+//       <h1 className="cat-card-section-title">Featured Categories</h1>
+//       <div className="cat-card-container">
+//         <MDBCarousel showIndicators showControls fade>
+//           {catArr.map((item) => (
+//             <MDBCarouselItem key={item.id} itemId={item.id}>
+//               <img src={item.img} alt="..." />
+//               <MDBCarouselCaption>
+//                 <h5>{item.title}</h5>
+//                 <p>{item.desc}</p>
+//               </MDBCarouselCaption>
+//             </MDBCarouselItem>
+//           ))}
+//         </MDBCarousel>
+//       </div>
+//     </div>
+//   );
+// }
