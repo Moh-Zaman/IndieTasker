aee37319241a83ba5ae4d7d8f313b5d2f18537d3
Emma Blencowe <146027243+embleem@users.noreply.github.com>
2024-01-25 17:33:04 +0000

    General css (#103)
    * Search button and hero banner responsiveness
    
    * Wrapped Hero in Bootstrap card, but run out of time to trouble shoot
    
    * Made global h1 font Montserrat for headings
    
    * Tried to fix hero again, adjusted font weights
    
    * Changed Navbar Colours to match colour palette

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ function Navbar() {
 
   return (
     <nav className="navigation">
-      <div className="brand-name">IndieTasker</div>
+      <h1 className="brand-name">IndieTasker</h1>
       <button
         className="hamburger"
         onClick={toggleNav}
